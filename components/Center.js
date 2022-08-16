4140399a4d3e58fdfc7ce069653edb047ac6a7f2
Ali Geshani <Triton.halley@outlook.com>
2022-08-16 18:01:18 +0430

    get playlist data from spotify api , add header cover and move logout button to header on user avatar and add Songs component to display playlists songs

--- components/Center.js
+++ components/Center.js
@@ -1,7 +1,11 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
 const colors = [
   "from-indigo-500",
   "from-blue-500",
@@ -13,19 +17,31 @@ const colors = [
 
 function Center() {
   const { data: session } = useSession();
+  const spotifyApi = useSpotify();
   const [color, setColor] = useState(colors[0]);
-
+  const playlistId = useRecoilValue(playlistIdState);
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
   useEffect(() => {
     setColor(shuffle(colors).pop());
-  }, []);
+  }, [playlistId]);
 
+  useEffect(() => {
+    spotifyApi
+      .getPlaylist(playlistId)
+      .then((data) => {
+        setPlaylist(data.body);
+      })
+      .catch((err) => console.log("something went wrong!", err));
+  }, [spotifyApi, playlistId]);
+  console.log(playlist);
   return (
     <>
-      <div className="flex-grow text-white">
+      <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
         <header className="absolute top-5 right-8">
           <div
-            className="flex items-center bg-red-300 space-x-3 opacity-90 
-          hover:opacity-80 cursor-pointer rounded-full p-1 pr-2"
+            className="flex items-center bg-black  space-x-3 opacity-90 
+          hover:opacity-80 cursor-pointer rounded-full text-white p-1 pr-2"
+            onClick={signOut}
           >
             <img
               src={session?.user.image}
@@ -38,10 +54,23 @@ function Center() {
         </header>
         <section
           className={`flex items-end space-x-7 bg-gradient-to-b
-         to-black ${color} h-80 text-white padding-8`}
+         to-black ${color} h-80 text-white p-8`}
         >
-          <h1>hell</h1>
+          <img
+            className="h-44 w-44 shadow-2xl"
+            src={playlist?.images?.[0]?.url}
+            alt="playlist cover"
+          />
+          <div>
+            <p>PLAYLIST</p>
+            <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold ">
+              {playlist?.name}
+            </h1>
+          </div>
         </section>
+        <div>
+          <Songs />
+        </div>
       </div>
     </>
   );
