8504f6999a7aa167028e2e5f9efc962ae01bcf1a
Ali Geshani <Triton.halley@outlook.com>
2022-08-16 17:53:02 +0430

    remove logout button and make a playlist clickable and every playlist when clicked update atom state and that atom update Center component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,13 +8,17 @@ import {
 } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { playlistIdState } from "../atoms/playlistAtom";
 import useSpotify from "../hooks/useSpotify";
 let flag = false;
 function Sidebar() {
   const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
   const [playlist, setPlaylists] = useState([]);
+  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
 
+  console.log(playlistId);
   useEffect(() => {
     console.log(spotifyApi.getAccessToken(), session);
     if (spotifyApi.getAccessToken()) {
@@ -27,15 +31,11 @@ function Sidebar() {
 
   return (
     <>
-      <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
+      <div
+        className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 
+      overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex"
+      >
         <div className="space-y-4">
-          <button
-            className="flex items-center space-x-2 hover:text-white"
-            onClick={() => signOut()}
-          >
-            <HomeIcon className="h-5 w-5" />
-            <p>Logout</p>
-          </button>
           <button className="flex items-center space-x-2 hover:text-white">
             <HomeIcon className="h-5 w-5" />
             <p>Home</p>
@@ -65,7 +65,11 @@ function Sidebar() {
           {/* PlayLists */}
           {playlist.map((playlist) => {
             return (
-              <p key={playlist.id} className="cursor-pointer hover:text-white">
+              <p
+                key={playlist.id}
+                onClick={() => setPlaylistId(playlist.id)}
+                className="cursor-pointer hover:text-white"
+              >
                 {playlist.name}
               </p>
             );
